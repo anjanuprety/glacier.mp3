4faa65854f3885af60e455a7b6e1c4f34968ff3d
Anjan Upreti <anjanuprety787@gmail.com>
2024-09-12 16:56:38 +0545

    fixed the bug //declaring array as a function when rendering songs

--- mp3script.js
+++ mp3script.js
@@ -97,7 +97,7 @@ let userData={
 
 };
 //creating a function to display songs in the UI
-const renderSongs= array =>{ 
+const renderSongs= (array) =>{ 
    //The map() method is used to iterate through an array and return a new array. 
   const songsHTML = array.map((song) => {
     return `<li id="song-${song.id}" class="playlist-song">
@@ -110,7 +110,7 @@ const renderSongs= array =>{
           <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
       </button>
     </li>`
-  }).join(" ") //concatenating all elements of an array into a single string kinda like lists;
+  }).join("") //concatenating all elements of an array into a single string kinda like lists;
 
   //updating HTML elements to display the songs
   playlistSongs.innerHTML= songsHTML;
@@ -145,7 +145,7 @@ const getCurrentSongIndex=()=>{
   }
 
   //passing alphabetically sorted songs data to display in the UI
-  renderSongs(sortSongs);
+  renderSongs(sortSongs());
 
 
 const playSong= id =>{
