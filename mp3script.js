de83d689d235337c3464af438f69122a4ab0f971
Anjan Upreti <anjanuprety787@gmail.com>
2024-09-12 17:47:30 +0545

    removed audio files from main branch

--- mp3script.js
+++ mp3script.js
@@ -10,23 +10,23 @@ const shuffleButton = document.getElementById("shuffle");
 const allSongs=[
   {
     id: 0,
-    title: "Oceans",
-    artist: "Seafret",
-    duration: "3:35",
-    src: "https://drive.google.com/file/d/1IEe3MqIY-GmzNzONjN_rU-NV0gTsIvl3/view?usp=drive_link",
+    title: "Scratching The Surface",
+    artist: "Quincy Larson",
+    duration: "4:25",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/scratching-the-surface.mp3",
     
   },
   {
     id: 1,
-    title: "To be Loved",
-    artist: "Adele",
-    duration: "6:45",
-    src: "",
+    title: "Can't Stay Down",
+    artist: "Quincy Larson",
+    duration: "4:15",
+    src: "https://cdn.freecodecamp.org/curriculum/js-music-player/can't-stay-down.mp3",
 
   },
   {
     id: 2,
-    title: "",
+    title: "Still Learning",
     artist: "Quincy Larson",
     duration: "3:51",
     src: "https://cdn.freecodecamp.org/curriculum/js-music-player/still-learning.mp3",
